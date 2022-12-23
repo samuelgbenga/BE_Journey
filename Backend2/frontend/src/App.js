cfa9f1d66deedabb1c4b6596d207273dc3cd9d43
samuelgbenga <samuelgbenga972@gmail.com>
2022-12-23 05:56:24 +0100

    last for the night

--- Backend2/frontend/src/App.js
+++ Backend2/frontend/src/App.js
@@ -1,5 +1,23 @@
+import Todo from "./components/todo";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Todo App</h1>
+
+        <div className="top">
+          <input type="text" placeholder="Add todos..."></input>
+          <div className="add">Add</div>
+        </div>
+        <div className="list">
+          <Todo />
+          <Todo />
+          <Todo />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
