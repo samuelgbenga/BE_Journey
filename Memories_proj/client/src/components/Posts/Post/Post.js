c2ceb5f91b4cb8196f32b424b21f173a6805f742
samuelgbenga <samuelgbenga972@gmail.com>
2023-01-04 05:49:36 +0100

    donot ever use audit fix

--- Memories_proj/client/src/components/Posts/Post/Post.js
+++ Memories_proj/client/src/components/Posts/Post/Post.js
@@ -1,14 +1,40 @@
 import React from "react";
 import useStyles from "./styles";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
+import {
+  ThumbUpAlt,
+  Delete,
+  MoreHoriz,
+  ModeComment,
+} from "@mui/icons-material";
+import moment from "moment";
 
-const Post = () => {
-  const posts = useSelector((state) => state.posts);
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+
+const Post = ({ post }) => {
+  // const posts = useSelector((state) => state.posts);
   const classes = useStyles();
-  console.log(posts);
+
   return (
     <div>
-      <h1>Post</h1>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={post.selectedFile}
+          title={post.title}
+        />
+        <div className={classes.overlay}>
+          <Typography variant="h6">{post.creator}</Typography>
+          <Typography variant="body2"></Typography>
+        </div>
+      </Card>
     </div>
   );
 };
--- Memories_proj/client/src/components/Posts/Posts.js
+++ Memories_proj/client/src/components/Posts/Posts.js
@@ -1,15 +1,28 @@
 import React from "react";
 import Post from "./Post/Post";
+import { Grid, CircularProgress } from "@mui/material";
 import useStyles from "./styles";
+import { useSelector } from "react-redux";
 
 const Posts = () => {
   const classes = useStyles();
-  return (
-    <div>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </div>
+  const posts = useSelector((state) => state.posts);
+  console.log("from post component", posts);
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.mainContainer}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
